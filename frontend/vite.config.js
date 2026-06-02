255b6f0625f9159063e6a324894cf6d376c1276c
Saish Mekal <174009056+saishmekal@users.noreply.github.com>
2026-06-02 12:54:46 +0530

    Updated file

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,9 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  build: {
-    outDir: '../backend/dist',
-    emptyOutDir: true,
-  },
 })
 
